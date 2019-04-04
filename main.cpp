cf5ea61b579baa702cb54354c9cd7a5bb7ba39f5
Błażej Sowa <bsowa123@gmail.com>
2019-04-04 15:41:38 +0200

    add pwm subscribers

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include "ros.h"
 #include "geometry_msgs/Twist.h"
 #include "std_msgs/Int16.h"
+#include "std_msgs/UInt16MultiArray.h"
 #include "sensor_msgs/BatteryState.h"
 
 #include "diff_controller.h"
@@ -21,56 +22,38 @@ ros::Publisher *odom_pub;
 
 ros::Subscriber<geometry_msgs::Twist> *twist_sub;
 
-ros::Subscriber<std_msgs::Int16> *servo1_angle_sub;
-ros::Subscriber<std_msgs::Int16> *servo2_angle_sub;
-ros::Subscriber<std_msgs::Int16> *servo3_angle_sub;
-ros::Subscriber<std_msgs::Int16> *servo4_angle_sub;
-ros::Subscriber<std_msgs::Int16> *servo5_angle_sub;
-ros::Subscriber<std_msgs::Int16> *servo6_angle_sub;
-ros::Subscriber<std_msgs::Int16> *servo1_pwm_sub;
-ros::Subscriber<std_msgs::Int16> *servo2_pwm_sub;
-ros::Subscriber<std_msgs::Int16> *servo3_pwm_sub;
-ros::Subscriber<std_msgs::Int16> *servo4_pwm_sub;
-ros::Subscriber<std_msgs::Int16> *servo5_pwm_sub;
-ros::Subscriber<std_msgs::Int16> *servo6_pwm_sub;
-
 DiffController dc;
 
-void servo1AngleCallback(const std_msgs::Int16& msg)
+class ServoWrapper
 {
-	hServo.servo1.rotAbs(msg.data);
-	Serial.printf("[servo1AngleCallback] angle: %d\r\n", msg.data);
-}
+	int num;
+	IServo& servo;
 
-void servo2AngleCallback(const std_msgs::Int16& msg)
-{
-	hServo.servo2.rotAbs(msg.data);
-	Serial.printf("[servo2AngleCallback] angle: %d\r\n", msg.data);
-}
+public:
+	ServoWrapper(int num, IServo& servo)
+		: num(num),
+		  servo(servo) {}
 
-void servo3AngleCallback(const std_msgs::Int16& msg)
-{
-	hServo.servo3.rotAbs(msg.data);
-	Serial.printf("[servo3AngleCallback] angle: %d\r\n", msg.data);
-}
+	void angleCallback(const std_msgs::Int16& msg)
+	{
+		servo.rotAbs(msg.data);
+		Serial.printf("[servo%dAngleCallback] angle: %d\r\n", num, msg.data);
+	}
 
-void servo4AngleCallback(const std_msgs::Int16& msg)
-{
-	hServo.servo4.rotAbs(msg.data);
-	Serial.printf("[servo4AngleCallback] angle: %d\r\n", msg.data);
-}
+	void pwmCallback(const std_msgs::UInt16MultiArray& msg)
+	{
+		servo.setPeriod(msg.data[0]);
+		servo.setWidth(msg.data[1]);
+		Serial.printf("[servo%dPWMCallback] period: %d width: %d\r\n", num, msg.data[0], msg.data[1]);
+	}
+};
 
-void servo5AngleCallback(const std_msgs::Int16& msg)
-{
-	hServo.servo5.rotAbs(msg.data);
-	Serial.printf("[servo5AngleCallback] angle: %d\r\n", msg.data);
-}
-
-void servo6AngleCallback(const std_msgs::Int16& msg)
-{
-	hServo.servo6.rotAbs(msg.data);
-	Serial.printf("[servo6AngleCallback] angle: %d\r\n", msg.data);
-}
+ServoWrapper servo1(1, hServo.servo1);
+ServoWrapper servo2(2, hServo.servo2);
+ServoWrapper servo3(3, hServo.servo3);
+ServoWrapper servo4(4, hServo.servo4);
+ServoWrapper servo5(5, hServo.servo5);
+ServoWrapper servo6(6, hServo.servo6);
 
 void cmdVelCallback(const geometry_msgs::Twist& msg)
 {
@@ -82,12 +65,33 @@ void initROS()
     battery_pub = new ros::Publisher("/battery", &battery);
 	odom_pub = new ros::Publisher("/odom", &odom);
 	twist_sub = new ros::Subscriber<geometry_msgs::Twist>("/cmd_vel", &cmdVelCallback);
-	servo1_angle_sub = new ros::Subscriber<std_msgs::Int16>("/servo1/angle", &servo1AngleCallback);
-	servo2_angle_sub = new ros::Subscriber<std_msgs::Int16>("/servo2/angle", &servo2AngleCallback);
-	servo3_angle_sub = new ros::Subscriber<std_msgs::Int16>("/servo3/angle", &servo3AngleCallback);
-	servo4_angle_sub = new ros::Subscriber<std_msgs::Int16>("/servo4/angle", &servo4AngleCallback);
-	servo5_angle_sub = new ros::Subscriber<std_msgs::Int16>("/servo5/angle", &servo5AngleCallback);
-	servo6_angle_sub = new ros::Subscriber<std_msgs::Int16>("/servo6/angle", &servo6AngleCallback);
+
+	ros::Subscriber<std_msgs::Int16, ServoWrapper> *servo1_angle_sub = 
+		new ros::Subscriber<std_msgs::Int16, ServoWrapper>("/servo1/angle", &ServoWrapper::angleCallback, &servo1);
+	ros::Subscriber<std_msgs::Int16, ServoWrapper> *servo2_angle_sub = 
+		new ros::Subscriber<std_msgs::Int16, ServoWrapper>("/servo2/angle", &ServoWrapper::angleCallback, &servo2);
+	ros::Subscriber<std_msgs::Int16, ServoWrapper> *servo3_angle_sub = 
+		new ros::Subscriber<std_msgs::Int16, ServoWrapper>("/servo3/angle", &ServoWrapper::angleCallback, &servo3);
+	ros::Subscriber<std_msgs::Int16, ServoWrapper> *servo4_angle_sub = 
+		new ros::Subscriber<std_msgs::Int16, ServoWrapper>("/servo4/angle", &ServoWrapper::angleCallback, &servo4);
+	ros::Subscriber<std_msgs::Int16, ServoWrapper> *servo5_angle_sub = 
+		new ros::Subscriber<std_msgs::Int16, ServoWrapper>("/servo5/angle", &ServoWrapper::angleCallback, &servo5);
+	ros::Subscriber<std_msgs::Int16, ServoWrapper> *servo6_angle_sub = 
+		new ros::Subscriber<std_msgs::Int16, ServoWrapper>("/servo6/angle", &ServoWrapper::angleCallback, &servo6);
+
+	ros::Subscriber<std_msgs::UInt16MultiArray, ServoWrapper> *servo1_pwm_sub =
+		new ros::Subscriber<std_msgs::UInt16MultiArray, ServoWrapper>("/servo1/pwm", &ServoWrapper::pwmCallback, &servo1);
+	ros::Subscriber<std_msgs::UInt16MultiArray, ServoWrapper> *servo2_pwm_sub =
+		new ros::Subscriber<std_msgs::UInt16MultiArray, ServoWrapper>("/servo2/pwm", &ServoWrapper::pwmCallback, &servo2);
+	ros::Subscriber<std_msgs::UInt16MultiArray, ServoWrapper> *servo3_pwm_sub =
+		new ros::Subscriber<std_msgs::UInt16MultiArray, ServoWrapper>("/servo3/pwm", &ServoWrapper::pwmCallback, &servo3);
+	ros::Subscriber<std_msgs::UInt16MultiArray, ServoWrapper> *servo4_pwm_sub =
+		new ros::Subscriber<std_msgs::UInt16MultiArray, ServoWrapper>("/servo4/pwm", &ServoWrapper::pwmCallback, &servo4);
+	ros::Subscriber<std_msgs::UInt16MultiArray, ServoWrapper> *servo5_pwm_sub =
+		new ros::Subscriber<std_msgs::UInt16MultiArray, ServoWrapper>("/servo5/pwm", &ServoWrapper::pwmCallback, &servo5);
+	ros::Subscriber<std_msgs::UInt16MultiArray, ServoWrapper> *servo6_pwm_sub =
+		new ros::Subscriber<std_msgs::UInt16MultiArray, ServoWrapper>("/servo6/pwm", &ServoWrapper::pwmCallback, &servo6);
+
     nh.advertise(*battery_pub);
 	nh.advertise(*odom_pub);
 	nh.subscribe(*twist_sub);
@@ -97,6 +101,12 @@ void initROS()
 	nh.subscribe(*servo4_angle_sub);
 	nh.subscribe(*servo5_angle_sub);
 	nh.subscribe(*servo6_angle_sub);
+	nh.subscribe(*servo1_pwm_sub);
+	nh.subscribe(*servo2_pwm_sub);
+	nh.subscribe(*servo3_pwm_sub);
+	nh.subscribe(*servo4_pwm_sub);
+	nh.subscribe(*servo5_pwm_sub);
+	nh.subscribe(*servo6_pwm_sub);
 }
 
 void setupServos()
@@ -138,6 +148,7 @@ void batteryLoop()
     long dt = 1000;
     while(true)
     {
+		nh.spinOnce();
         battery.voltage = sys.getSupplyVoltage();
         battery_pub->publish(&battery);
         sys.delaySync(t, dt);
@@ -147,9 +158,10 @@ void batteryLoop()
 void odomLoop()
 {
     uint32_t t = sys.getRefTime();
-    long dt = 50;
+    long dt = 500;
     while(true)
     {
+		nh.spinOnce();
 		std::vector<float> odo = dc.getOdom();
 		odom.linear.x = odo[0];
 		odom.angular.z = odo[1];
