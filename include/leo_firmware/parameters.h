61d86777851c708049ebb09a55d5168eb421cffe
Błażej Sowa <bsowa123@gmail.com>
2021-04-20 20:30:55 +0200

    Add F suffix to float literals in parameters defaults

--- include/leo_firmware/parameters.h
+++ include/leo_firmware/parameters.h
@@ -19,20 +19,20 @@ struct Parameters {
   int servo_width_max[6] = {2000, 2000, 2000, 2000, 2000, 2000};
 
   // Motor
-  float motor_encoder_resolution = 878.4;
+  float motor_encoder_resolution = 878.4F;
   int motor_encoder_pullup = 1;
-  float motor_max_speed = 800.0;
-  float motor_pid_p = 0.0;
-  float motor_pid_i = 0.005;
-  float motor_pid_d = 0.0;
-  float motor_power_limit = 1000.0;
-  float motor_torque_limit = 1000.0;
+  float motor_max_speed = 800.0F;
+  float motor_pid_p = 0.0F;
+  float motor_pid_i = 0.005F;
+  float motor_pid_d = 0.0F;
+  float motor_power_limit = 1000.0F;
+  float motor_torque_limit = 1000.0F;
 
   // Differential drive
-  float dd_wheel_radius = 0.0625;
-  float dd_wheel_separation = 0.33;
-  float dd_angular_velocity_multiplier = 1.91;
-  float dd_input_timeout = 500;
+  float dd_wheel_radius = 0.0625F;
+  float dd_wheel_separation = 0.33F;
+  float dd_angular_velocity_multiplier = 1.91F;
+  float dd_input_timeout = 500.0F;
 
   void load(ros::NodeHandle &nh);
 };
