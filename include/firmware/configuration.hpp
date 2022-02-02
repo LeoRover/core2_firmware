c01a6416d9410f43869bcdc6e56a499d0afcdf24
Błażej Sowa <bsowa123@gmail.com>
2022-02-02 17:59:51 +0100

    Update comments in configuration

--- include/firmware/configuration.hpp
+++ include/firmware/configuration.hpp
@@ -11,12 +11,7 @@ const char *const FIRMWARE_VERSION = "2.0.0";
 // Set to either hSens1 or hSens2
 static hFramework::hSensor_i2c &IMU_HSENS = hFramework::hSens2;
 
-// The hSens port to which the GPS is connected
-// Set to either hSens3 or hSens4
-static hFramework::hSensor_serial &GPS_HSENS = hFramework::hSens3;
-
 // The pin which will be used to drive the informative LED on the power switch
-// By default it is set to pin1 on hExt port
 static hFramework::hGPIO &LED = hFramework::hExt.pin1;
 
 // Number of encoder readings to remember when estimating the wheel velocity
@@ -29,7 +24,7 @@ const uint32_t BATTERY_BUFFER_SIZE = 3000;
 const uint16_t UPDATE_PERIOD = 10;
 
 // The periods (in number of calls to the update() function), at which different
-// data is publihed on the ROS topics
+// data is published on the ROS topics
 const uint8_t BATTERY_PUB_PERIOD = 10;
 const uint8_t JOINTS_PUB_PERIOD = 5;
 const uint8_t ODOM_PUB_PERIOD = 5;
